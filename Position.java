0243a9765544e9bb2524063d6d0c432b25405559
George Zheng <georgezheng999@gmail.com>
2018-01-08 15:06:02 -0500

    added constructor and accessor methods;git push

---
+++ Position.java
@@ -0,0 +1,23 @@
+public class Position{
+	
+	private int xPosition;
+	private int yPosition;
+
+	public Position(int x, int y){
+		xPosition = x;
+		yPosition = y;
+	}
+
+	public boolean isOccupied(){
+
+	}
+
+	public int getX(){
+
+	}
+
+	public int getY(){
+		
+	}
+
+}
