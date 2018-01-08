0720c861986ab41cdc65b1a1d99c61a695b1ace2
George Zheng <georgezheng999@gmail.com>
2018-01-08 15:12:30 -0500

    added queen class

---
+++ King.java
@@ -0,0 +1,14 @@
+public class King{
+	
+	private int color;
+	private position Position;
+
+	public boolean validMove(){
+
+	}
+
+	public boolean move(){
+		
+	}
+
+}
---
+++ Queen.java
@@ -0,0 +1,14 @@
+public class Queen{
+	
+	private int color;
+	private position Position;
+
+	public boolean validMove(){
+
+	}
+
+	public boolean move(){
+		
+	}
+
+}
