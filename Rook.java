15c7f1b4fd8508be6fc3d0c88f700e94e511b44b
Anne Zhang <azhang14@stuy.edu>
2018-01-09 02:23:39 -0500

    check other pieces for more info

--- Rook.java
+++ Rook.java
@@ -1,14 +1,28 @@
-public class Rook{
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+public class Rook extends Pieces{
 	
-	private int color;
-	private position Position;
+	/*private int color;
+	private position Position;*/
+	public Rook(int x, int y, int colors){
+		super(x,y,colors);
+	}
+	
+	public Rook(Position coordinate, int colors){
+		super(coordinate,colors);
+	}
 
-	public boolean validMove(){
-
+	//figuring out return statements
+	/*public boolean validMove(){
+		
 	}
 
 	public boolean move(){
 		
+	}*/
+	public String toString(){
+		return "";
 	}
 
 }
