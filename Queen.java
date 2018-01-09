805d58e683c1703e8b6ed452ed2ffc37dccc15b5
Anne Zhang <azhang14@stuy.edu>
2018-01-09 02:09:22 -0500

    changed it so that it extends Pieces and placed all the applicable variables

--- Queen.java
+++ Queen.java
@@ -1,14 +1,28 @@
-public class Queen{
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+public class Queen extends Pieces{
 	
-	private int color;
-	private position Position;
+	/*private int color;
+	private position Position;*/
+	public Queen(int x, int y, int colors){
+		super(x,y,colors);
+	}
+	
+	public Queen(Position coordinate, int colors){
+		super(coordinate,colors);
+	}
 
-	public boolean validMove(){
-
+	//figuring out return statements
+	/*public boolean validMove(){
+		
 	}
 
 	public boolean move(){
 		
+	}*/
+	public String toString(){
+		return "";
 	}
 
 }
