94009e8f10555fa8b5f56c489ddbece2402d6fff
Anne Zhang <azhang14@stuy.edu>
2018-01-09 02:16:03 -0500

    border is made in the Board still trying to figure out how to make the white and black checks

---
+++ Chessboard.java
@@ -0,0 +1,67 @@
+// help from https://stackoverflow.com/questions/21077322/create-a-chess-board-with-jpanel
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.swing.*;
+import javax.swing.border.*;
+
+public class Chessboard {
+
+    private final JPanel gui = new JPanel(new BorderLayout(3, 3));
+    private JButton[][] chessBoardSquares = new JButton[8][8];
+    private JPanel chessBoard;
+    private final JLabel message = new JLabel(
+            "Chess Champ is ready to play!");
+    private static final String COLS = "ABCDEFGH";
+
+    Chessboard() {
+        initializeGui();
+    }
+
+    public final void initializeGui() {
+        // set up the main GUI
+        gui.setBorder(new EmptyBorder(5, 5, 5, 5));
+        JToolBar tools = new JToolBar();
+        tools.setFloatable(false);
+        gui.add(tools, BorderLayout.PAGE_START);
+        
+       
+
+        chessBoard = new JPanel(new GridLayout(0, 9));
+        chessBoard.setBorder(new LineBorder(Color.BLACK));
+        gui.add(chessBoard);
+
+
+        //fill the chess board
+        chessBoard.add(new JLabel(""));
+        // fill the top row
+        
+    }
+
+    public final JComponent getChessBoard() {
+        return chessBoard;
+    }
+
+    public final JComponent getGui() {
+        return gui;
+    }
+
+    public static void main(String[] args) {
+        Runnable r = new Runnable() {
+
+            @Override
+            public void run() {
+                Chessboard cb =
+                        new Chessboard();
+
+                JFrame f = new JFrame("PLAY CHESS");
+                f.add(cb.getGui());
+                f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                f.setLocationByPlatform(true);
+                f.pack();
+                f.setMinimumSize(f.getSize());
+                f.setVisible(true);
+            }
+        };
+        SwingUtilities.invokeLater(r);
+    }
+}
