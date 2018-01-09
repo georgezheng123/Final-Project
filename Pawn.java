c682f31458f7df4edaa792be9eba4911b179c0fe
Anne Zhang <azhang14@stuy.edu>
2018-01-09 02:19:35 -0500

    did the same thing that I did to Queen -> made it extend Pieces...

--- Pawn.java
+++ Pawn.java
@@ -1,14 +1,28 @@
-public class Pawn{
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+public class Pawn extends Pieces{
 	
-	private int color;
-	private position Position;
+	/*private int color;
+	private position Position;*/
+	public Pawn(int x, int y, int colors){
+		super(x,y,colors);
+	}
+	
+	public Pawn(Position coordinate, int colors){
+		super(coordinate,colors);
+	}
 
-	public boolean validMove(){
-
+	//figuring out return statements
+	/*public boolean validMove(){
+		
 	}
 
 	public boolean move(){
 		
+	}*/
+	public String toString(){
+		return "";
 	}
 
 }
