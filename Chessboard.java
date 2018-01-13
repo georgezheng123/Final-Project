1919b27a4d42ff21aa4ef539a082de0a8ee2adfb
Anne Zhang <azhang14@stuy.edu>
2018-01-12 19:47:35 -0500

    picture problems

--- Chessboard.java
+++ Chessboard.java
@@ -4,30 +4,46 @@ import java.awt.event.*;
 import java.awt.Color;
 import javax.swing.border.*;
 import java.util.ArrayList;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+//import javax.swing.ImageIcon;
+import java.io.*;
 
 class Chessboard extends JFrame{
 
     private Pieces[][] pieces;
     private JPanel panel;
-    //private static ArrayList<Pieces> White;
-    //private static ArrayList<Pieces> Black;
+    private ImageIcon BlackRook,WhiteRook,BlackQueen,WhiteQueen,BlackPawn,WhitePawn,BlackKing,WhiteKing,BlackKnight,WhiteKnight,BlackBishop,WhiteBishop;
+    private JLabel addBlackRook;
+    private static ArrayList<Pieces> White;
+    private static ArrayList<Pieces> Black;
     public static Border blackBorder = new LineBorder(Color.black);
-
     public static void main(String[] args){
         Chessboard a = new Chessboard();
-        
+        a.setVisible(true);
     }
 
     public Chessboard() {
         this.pieces = new Pieces[8][8];
         this.panel = new JPanel(new GridLayout(8, 8));
-		//Black = new ArrayList<Pieces>();
-		//White = new ArrayList<Pieces>();
+		Black = new ArrayList<Pieces>();
+		White = new ArrayList<Pieces>();
 		boolean backGround = false;
 		for (int row = 0; row < pieces.length; row++){
 			for(int col = 0; col < pieces[row].length; col++){
 				Pieces newpiece = new Pieces(row, col, 2);
-				
+				if (row == 0){
+					if (col == 0 || col == 7){
+						BlackRook = new ImageIcon(new ImageIcon("BlackRook.png").getImage().getScaledInstance(100,100, Image.SCALE_SMOOTH));
+						addBlackRook = new JLabel(BlackRook);
+						add(addBlackRook);
+						
+						}
+					/*else{
+						WhiteRook = new ImageIcon(new ImageIcon("./Pieces/WhiteRook.png").getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH));
+						}*/
+					Black.add(newpiece);
+					}
 				pieces[row][col] = newpiece;
 				newpiece.setOpaque(true);
 				newpiece.setBorder(blackBorder);
