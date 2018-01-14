5b8d56377c55198d86dfe53b2ed874f7b32946be
Anne Zhang <azhang14@stuy.edu>
2018-01-14 14:06:32 -0500

    changed Pieces to not use JLabel

--- Pieces.java
+++ Pieces.java
@@ -1,31 +1,30 @@
-import javax.swing.*;
-import java.util.ArrayList;
-
-
-public class Pieces extends JLabel{
-	private Position position;
-	private int color;
-	
-	public Pieces(Position coordinate, int colors){
-		position = coordinate;
-		color = colors;
-	}
-	
-	
-	public Pieces(int x, int y, int colors){
-		position = new Position(x,y);
-		color = colors;
-	}
-	
-	public Position getPos(){
-		return position;
-	}
-	
-	public void setPos(Position coordinate){
-		position = coordinate;
-	}
-	
-	public String toString(){
+import javax.swing.*; 
+import java.util.ArrayList; 
+public class Pieces { 
+	private Position position; 
+	private int color; 
+	private JLabel getLabel; 
+	private JLabel setLabel; 
+	public Pieces(Position coordinate, int colors){ 
+		position = coordinate; color = colors; 
+	} 
+	public JLabel getJLabel(){ 
+		return getLabel; 
+	} 
+	public JLabel setJLabel(){ 
+		return setLabel; 
+	} 
+	public Pieces(int x, int y, int colors){ 
+		position = new Position(x,y); color = colors; 
+	} 
+	public Position getPos(){ 
+		return position; 
+	} 
+	public void setPos(Position coordinate){ 
+		position = coordinate; 
+	} 
+	public String toString(){ 
 		return " "; 
-	}
+	} 
 }
+	
