a82d5c7ee4581b6c937673610c05955b7ee80497
George Zheng <georgezheng999@gmail.com>
2018-01-16 19:04:02 -0500

    added validate function

--- King.java
+++ King.java
@@ -6,9 +6,9 @@ public class King extends Pieces{
 
 	public static validate(int[] from, int[] to){
 		int fromX = from[0];
-		int fromY = from[0];
-		int toX = from[1];
-		int toY = from[1];
+		int fromY = from[1];
+		int toX = to[0];
+		int toY = to[1];
 	}
 
 
