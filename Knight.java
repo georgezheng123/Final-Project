3d0429586a98d0097ebd319a7fa5ae390f471e30
George Zheng <georgezheng999@gmail.com>
2018-01-17 17:07:40 -0500

    added knight movement

--- Knight.java
+++ Knight.java
@@ -1,20 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+
 public class Knight extends Pieces{
 	
 
-	public Knight(int x, int y, int colors){
-		super(x,y,colors);
-	}
-	
-	public Knight(Position coordinate, int colors){
-		super(coordinate,colors);
-	}
+	public static boolean validate(String color, int[] from, int[] to){
+		int fromX = from[0];
+		int fromY = from[1];
+		int toX = to[0];
+		int toY = to[1];
 
+		double deltaX = Math.abs(fromX - toX);
+		double deltaY = Math.abs(fromY - toY);
 
-	public String toString(){
-		return "";
+		Boolean sleepingL = deltaX == 2.0 && deltaY == 1.0; 
+		Boolean upwardsL = deltaX == 1.0 && deltaY == 2.0;
+
+		return sleepingL || upwardsL;
 	}
 
 }
