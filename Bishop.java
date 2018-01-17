4e91ed90e6df8a1bdc07ec853d344cde4fe1e2ad
George Zheng <georgezheng999@gmail.com>
2018-01-17 16:47:09 -0500

    bishop movement

--- Bishop.java
+++ Bishop.java
@@ -1,14 +1,17 @@
 public class Bishop{
 	
-	private int color;
-	private position Position;
 
-	public boolean validMove(){
+	public static boolean validate(String color, int[] from, int[] to){
+		int fromX = from[0];
+		int fromY = from[1];
+		int toX = to[0];
+		int toY = to[1];
 
+		double deltaX = fromX - toX;
+		double deltaY = fromY - toY;
+
+		return deltaX == deltaY && deltaX != 0;
 	}
 
-	public boolean move(){
-		
-	}
 
 }
