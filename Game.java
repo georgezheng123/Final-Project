cd2dcabfdef6a404f55f688a67c1389ef3889206
George Zheng <georgezheng999@gmail.com>
2018-01-17 19:24:21 -0500

    unneeded files in game

--- Game.java
+++ Game.java
@@ -5,15 +5,6 @@ public class Game{//deprecated
 	private Chessboard ChessBoard;
 	private ArrayList listOfMoves;
 
-	public Game(){
-		Chessboard a = new Chessboard();
-		ChessBoard = a;
-	}
-
-	public void endGame(){
-		
-	}
-
 	public static void main(String[] args){
 		Game game = new Game();
 
