5f26537112e2feb1006ae9af770de9219ab02e1b
George Zheng <georgezheng999@gmail.com>
2018-01-19 20:26:07 -0500

    king can't move himself into check

--- King.java
+++ King.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.*;
 
 public class King extends Pieces{
 
-	public static boolean validate(String color, int[] from, int[] to){
+	public static boolean validate(String color, int[] from, int[] to, int[][] state){
 		int fromX = from[0];
 		int fromY = from[1];
 		int toX = to[0];
@@ -14,14 +15,21 @@ public class King extends Pieces{
 		double deltaY = fromY - toY;
 
 		double dist = Math.hypot(deltaX, deltaY);
+
+		isInCheck(state, to);
 		return dist == 1.0 || dist == Math.sqrt(2);
 	}
 
+	public static boolean isInCheck(int[][] state, int[] location){
+		    for (int[] i: state){
+        System.out.println(Arrays.toString(i));
+    }
+    System.out.println(Arrays.toString(location));
+		;
+		// if (){
 
-	// public boolean isInCheck(int[][] state, int[] king, ){
-	// 	if (){
-
-	// 	}
-	// }
+		// }
+		return false;
+	}
 
 }
