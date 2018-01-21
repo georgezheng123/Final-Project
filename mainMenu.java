d19ce522ad88a62e5bdd0958f078001d2e1f0d7a
Anne Zhang <azhang14@stuy.edu>
2018-01-21 00:47:39 -0500

    added skeleton for mainMenu

---
+++ mainMenu.java
@@ -0,0 +1,26 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class mainMenu{
+	private JFrame window;
+	private ImageIcon background;
+	private JButton start;
+	
+	public mainMenu(){
+	
+	
+	}
+
+
+
+
+
+
+
+}
