391200ec5dec1ba952aaaead8630bfd84d9ac58a
George Zheng <georgezheng999@gmail.com>
2018-01-21 11:48:17 -0500

    checking full operational

--- Chessboard.java
+++ Chessboard.java
@@ -5,12 +5,12 @@ import javax.swing.*;
 import java.io.*;
 import java.awt.image.BufferedImage;
 
-// check if king is in check, king can't move into check
+// king can't move into check
 // check if move other piece would mean king is in check
 // detect checkmate:: 
 // king is in check
 // look at his possible moves, loop through them to see if he'll still be in check
-// look at the piece checking him(more than 1 == checkmate), if some of his pieces can capture it
+// look at the piece checking him, if some of his pieces can capture it
 // look if some of his pieces can block that piece
 // it is checkmates
 // en passant, castling , pawn promotion
