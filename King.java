48057737464e9d75e13d755d0f4d9948f9c1882c
George Zheng <georgezheng999@gmail.com>
2018-01-21 20:05:24 -0500

    check if checking piece can be captured

--- King.java
+++ King.java
@@ -75,9 +75,11 @@ public class King extends Pieces{
 		System.out.println(otherColor(color) + "king cannot move away");
 		System.out.println("numb checking: " + numbCheckingPiece(otherColor(color), state, kingLocation, (color.equals("White")) ? -6 : 6));
 		if (1 < numbCheckingPiece(otherColor(color), state, kingLocation, (color.equals("White")) ? -6 : 6)){
-			System.out.println(otherColor(color) + "king cannot move away with more than one piece, this is checkmate");
+			System.out.println(otherColor(color) + "king cannot move away with more than one piece checking, this is checkmate");
 			return true;
 		}
+		System.out.println("only one piece is checking the king");
+		canCaptureChecking(otherColor(color), state, kingLocation, (color.equals("White")) ? -6 : 6);
 		return true;
 	}
 
@@ -91,6 +93,7 @@ public class King extends Pieces{
 				}
 			}
 		}
+
 		return false;
 	}
 
@@ -118,6 +121,32 @@ public class King extends Pieces{
 			}
 		}
 		return counter;
+	}
+
+	public static boolean canCaptureChecking(String color, int[][] state, int[] location, int type){
+		// for (int[] i: state){
+  //   	    System.out.println(Arrays.toString(i));
+  //   	}
+		color = otherColor(color);
+		for (int i=0; i<8; i++){
+			for (int j=0; j<8; j++){
+				int pieceID = state[j][i];
+				int[] from = new int[] {i,j};
+				if (pieceID * type < 0){
+					Boolean isValid;
+					if (Math.abs(pieceID) != 6){
+						isValid = Pieces.validates(true, color, pieceID, from, location, state);
+					}else{
+						isValid = validate(color, from, location, state, true);
+					}
+					if (isValid){
+						System.out.println(color + pieceID + "IS CHECKING TEH KING at " 
+							+ Arrays.toString(location) + " from "+ Arrays.toString(from));
+					}
+				}
+			}
+		}
+		return false;
 	}
 
 	public static String otherColor(String color){
