d8f58becfcf5e80c4b6554ac5cb2d1eb03eacaff
George Zheng <georgezheng999@gmail.com>
2018-01-22 01:08:23 -0500

    brought back turns

--- Chessboard.java
+++ Chessboard.java
@@ -181,11 +181,11 @@ public class Chessboard extends JFrame implements MouseListener, MouseMotionList
         moveX = parentLocation.x - e.getX();
         moveY = parentLocation.y - e.getY();
         chessPiece = (JLabel) c; 
-        // if (getColor(chessPiece).equals("White") && turn % 2 == 1){
-        //     return;
-        // }else if (getColor(chessPiece).equals("Black") && turn % 2 == 0){
-        //     return;
-        // }
+        if (getColor(chessPiece).equals("White") && turn % 2 == 1){
+            return;
+        }else if (getColor(chessPiece).equals("Black") && turn % 2 == 0){
+            return;
+        }
         if (this.checkmate){
                     infoBox("Checkmate!", "Checkmate!");
                     return;
@@ -209,11 +209,11 @@ public class Chessboard extends JFrame implements MouseListener, MouseMotionList
     public void mouseDragged(MouseEvent me)
     {
         if (chessPiece == null) return;
-        // if (getColor(chessPiece).equals("White") && turn % 2 == 1){
-        //     return;
-        // }else if (getColor(chessPiece).equals("Black") && turn % 2 == 0){
-        //     return;
-        // }
+        if (getColor(chessPiece).equals("White") && turn % 2 == 1){
+            return;
+        }else if (getColor(chessPiece).equals("Black") && turn % 2 == 0){
+            return;
+        }
         if (this.checkmate){
                     infoBox("Checkmate!", "Checkmate!");
                     return;
@@ -242,11 +242,11 @@ public class Chessboard extends JFrame implements MouseListener, MouseMotionList
 
         if (chessPiece == null) return;
 
-        // if (getColor(chessPiece).equals("White") && turn % 2 == 1){
-        //     return;
-        // }else if (getColor(chessPiece).equals("Black") && turn % 2 == 0){
-        //     return;
-        // }
+        if (getColor(chessPiece).equals("White") && turn % 2 == 1){
+            return;
+        }else if (getColor(chessPiece).equals("Black") && turn % 2 == 0){
+            return;
+        }
         if (this.checkmate){
                     infoBox("Checkmate!", "Checkmate!");
                     return;
