ac51cf76600b8e854c9a4ac6853798dae3005452
Anne Zhang <azhang14@stuy.edu>
2018-01-21 22:44:16 -0500

    start button now shows up but still having trouble making it change into the Chessboard

--- mainMenu.java
+++ mainMenu.java
@@ -3,50 +3,51 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.border.*;
-public class mainMenu extends JFrame{
-	private Container pane;
-	private JFrame window = new JFrame("Main Menu");
-	private JFrame startPage = new JFrame("Menu");
-	private JButton start = new JButton("Start");
-	private JLabel background;
+
+public class mainMenu extends JFrame {
+    private Container pane;
+    private JFrame window = new JFrame("Main Menu");
+    private JButton start = new JButton("Start");
+    private JLabel backImage;
 	
-	public mainMenu(){
-		pane = this.getContentPane();
-		pane.setLayout(new FlowLayout());
-		
-		start.addActionListener(new ActionListener() {
+    public mainMenu() {
+	
+	pane = this.getContentPane();
+	pane.setLayout(new FlowLayout());
+    
+	start.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
-		    startPage.dispose();
+		    window.dispose();
 		    new Chessboard();
 		}
 	    });
-		
-		background = new JLabel();
-		background.setLayout(new FlowLayout(FlowLayout.CENTER));
-		background.add(Box.createRigidArea(new Dimension(800,800)));
-		background.setIcon(new ImageIcon("mainMenu.png"));
-		
-		start.setPreferredSize(new Dimension(200,50));
-		start.setLocation(700,300);
-		start.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-		
-		background.add(start);
-		pane.add(background);
-		background.add(Box.createRigidArea(new Dimension(800,800)));
-		this.setVisible(true);
-		this.pack();
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setSize(800,800);
-}
-		public static void main(String[] args){
-			new mainMenu();
-		}
+
+	backImage = new JLabel();
+	backImage.setLayout(new FlowLayout(FlowLayout.CENTER));
+	backImage.add(Box.createRigidArea(new Dimension(1000,475)));
+
+	backImage.setIcon(new ImageIcon("mainMenu.png"));
+
+	start.setPreferredSize(new Dimension(200,50));
+	
+	start.setLocation(450,525);
+
+	start.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+
+	backImage.add(start);
+	backImage.add(Box.createRigidArea(new Dimension(800,800)));
+	pane.add(backImage);
+	this.pack();
+	this.setVisible(true);
 	
 
-
-
-
-
-
-
+	this.setTitle("CHESS");
+	this.setSize(800,800);
+	this.setLocation(100,10);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    
+    public static void main(String[] args) {
+	new mainMenu();
+    }
 }
